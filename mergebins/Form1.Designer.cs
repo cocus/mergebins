84ef5423052e3190a6026d4ec774bb5bdf63a0d8
Santiago Hormazabal <santiagohssl@gmail.com>
2021-08-22 20:32:25 -0300

    First commit

---
+++ mergebins/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace mergebins
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelOddPath = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelEvenPath = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonMerge = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Odd:";
+            // 
+            // labelOddPath
+            // 
+            this.labelOddPath.AllowDrop = true;
+            this.labelOddPath.AutoEllipsis = true;
+            this.labelOddPath.AutoSize = true;
+            this.labelOddPath.Location = new System.Drawing.Point(53, 0);
+            this.labelOddPath.Name = "labelOddPath";
+            this.labelOddPath.Size = new System.Drawing.Size(45, 13);
+            this.labelOddPath.TabIndex = 1;
+            this.labelOddPath.Text = "(not set)";
+            this.labelOddPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.generic_DragDrop);
+            this.labelOddPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.generic_DragEnter);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.buttonMerge, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelEvenPath, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelOddPath, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(643, 73);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // labelEvenPath
+            // 
+            this.labelEvenPath.AllowDrop = true;
+            this.labelEvenPath.AutoEllipsis = true;
+            this.labelEvenPath.AutoSize = true;
+            this.labelEvenPath.Location = new System.Drawing.Point(53, 13);
+            this.labelEvenPath.Name = "labelEvenPath";
+            this.labelEvenPath.Size = new System.Drawing.Size(45, 13);
+            this.labelEvenPath.TabIndex = 3;
+            this.labelEvenPath.Text = "(not set)";
+            this.labelEvenPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.generic_DragDrop);
+            this.labelEvenPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.generic_DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Even:";
+            // 
+            // buttonMerge
+            // 
+            this.buttonMerge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMerge.Location = new System.Drawing.Point(565, 47);
+            this.buttonMerge.Name = "buttonMerge";
+            this.buttonMerge.Size = new System.Drawing.Size(75, 23);
+            this.buttonMerge.TabIndex = 3;
+            this.buttonMerge.Text = "Merge!";
+            this.buttonMerge.UseVisualStyleBackColor = true;
+            this.buttonMerge.Click += new System.EventHandler(this.buttonMerge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 96);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(683, 135);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Merge Bins";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOddPath;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelEvenPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonMerge;
+    }
+}
+
---
+++ mergebins/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace mergebins
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static String BytesToString(long byteCount)
+        {
+            // https://stackoverflow.com/questions/281640/how-do-i-get-a-human-readable-file-size-in-bytes-abbreviation-using-net
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+
+        private void buttonMerge_Click(object sender, System.EventArgs e)
+        {
+            var even = labelEvenPath.Tag as string;
+            var odd = labelOddPath.Tag as string;
+            if (even == null || odd == null)
+            {
+                return;
+            }
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = System.IO.Path.GetFullPath(even);
+            saveFileDialog1.Title = "Save merged binary";
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "bin";
+            saveFileDialog1.Filter = "Binary files (*.bin)|*.bin|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            var output = saveFileDialog1.FileName;
+
+            byte[] evenFileBytes = System.IO.File.ReadAllBytes(even);
+            byte[] oddFileBytes = System.IO.File.ReadAllBytes(odd);
+
+            if (evenFileBytes.Length != oddFileBytes.Length)
+            {
+                return;
+            }
+
+            var inputSize = oddFileBytes.Length;
+
+            byte[] outputBytes = new byte[inputSize * 2];
+            for (int i = 0; i < inputSize; i++)
+            {
+                outputBytes[(i * 2) + 0] = evenFileBytes[i];
+                outputBytes[(i * 2) + 1] = oddFileBytes[i];
+            }
+
+            System.IO.File.WriteAllBytes(output, outputBytes);
+        }
+        private void generic_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void generic_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var file = files.First();
+            if (System.IO.File.Exists(file))
+            {
+                long size = new System.IO.FileInfo(file).Length;
+
+                ((Label)sender).Text = $"{file} ({BytesToString(size)})";
+                ((Label)sender).Tag = file;
+            }
+        }
+    }
+}
